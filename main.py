107436691970c1fd66244babf6c69e9d6cead15c
hossvadi <hossvadi@gmail.com>
2022-07-05 17:10:45 +0200

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 16:59:30 2022
+
+@author: Hossein.JvdZ
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("spotify_dataset.csv")
